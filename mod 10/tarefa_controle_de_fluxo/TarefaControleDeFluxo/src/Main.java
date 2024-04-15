196d5bdbaf1fa2d77ed7e1c23b192d8fe3907610
viit0r <vituuoliveira1@gmail.com>
2024-04-15 20:43:40 -0300

    Adicionando resolução da tarefa final do módulo 10

---
+++ mod 10/tarefa_controle_de_fluxo/TarefaControleDeFluxo/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> notas = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 1; i <= 4; i++) {
+            System.out.print("Digite a " + i + "° nota: ");
+            int notaDigitada = scanner.nextInt();
+            notas.add(notaDigitada);
+        }
+
+        int media = 0;
+
+        for (int nota : notas) {
+            media += nota;
+        }
+
+        media = media / notas.size();
+
+        System.out.println("Média do aluno: " + media);
+
+        if(media >= 7) {
+            System.out.println("Aluno aprovado!!");
+        }
+        else if (media >= 5) {
+            System.out.println("Aluno de recuperação!!");
+        }
+        else {
+            System.out.println("Aluno reprovado!!");
+        }
+    }
+}
