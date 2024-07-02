a5df31eb330e43357a215ef4b43ff0550a1394d3
viit0r <vituuoliveira1@gmail.com>
2024-07-02 19:40:40 -0300

    Adicionando a resolução da tarefa final do módulo 13

---
+++ mod 13/tarefa_heranca/TarefaHeranca/src/PessoaFisica.java
@@ -0,0 +1,20 @@
+public class PessoaFisica extends Pessoa {
+    private String cpf;
+    private String rg;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+}
